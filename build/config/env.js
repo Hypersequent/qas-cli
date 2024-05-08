e809983fa1c3e6f408435339b5aaa59c60861828
Ramil Amparo <ramilamparo@gmail.com>
2024-05-08 17:18:44 +0400

    gitignore build

--- build/config/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN = void 0;
-exports.TOKEN = process.env.QAS_TOKEN;
-//# sourceMappingURL=env.js.map
--- build/utils/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getApiUrl = void 0;
-const getApiUrl = (subdomain, zone) => `https://${subdomain}.${zone}.qasphere.com`;
-exports.getApiUrl = getApiUrl;
-//# sourceMappingURL=config.js.map
