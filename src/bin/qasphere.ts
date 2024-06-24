55b65f975a715c52b9b922bb1bd5fbaa9a227b86
Ramil Amparo <ramilamparo@gmail.com>
2024-06-24 13:38:21 +0400

    Warn instead of exiting.

--- src/bin/qasphere.ts
+++ src/bin/qasphere.ts
@@ -2,12 +2,12 @@
 
 import { hideBin } from 'yargs/helpers'
 import { run } from '../commands/main'
-import { printErrorThenExit, validateNodeVersion } from '../utils/misc'
+import { validateNodeVersion } from '../utils/misc'
 import { REQUIRED_NODE_VERSION } from '../utils/config'
 
 if (!validateNodeVersion()) {
-	printErrorThenExit(
-		`Node.js version must be v${REQUIRED_NODE_VERSION} or higher. Current version is ${process.version}`
+	console.warn(
+		`Node.js version must be v${REQUIRED_NODE_VERSION} or higher. Current version is ${process.version}. Some features may not work correctly.`
 	)
 }
 
