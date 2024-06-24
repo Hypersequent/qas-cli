d073cb172f96f6fd7ab3c8e72706d705911b7be6
Ramil Amparo <ramilamparo@gmail.com>
2024-06-24 15:18:45 +0400

    Remove comment.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const getApiUrl = (subdomain: string, zone: string) =>
 	`https://${subdomain}.${zone}.qasphere.com`
 
-export const REQUIRED_NODE_VERSION = '20.0.0' // lts/iron
+export const REQUIRED_NODE_VERSION = '20.0.0'
